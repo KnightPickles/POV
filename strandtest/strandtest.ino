22fa5ae7a24b6b7a9da6002689abd015355e827c
yama4272 <KnightPickles@Knights-MacBook-Pro.local>
2016-03-03 13:28:04 -0800

    interrupt additions

--- strandtest/strandtest.ino
+++ strandtest/strandtest.ino
@@ -1,20 +1,13 @@
-// Simple strand test for Adafruit Dot Star RGB LED strip.
-// This is a basic diagnostic tool, NOT a graphics demo...helps confirm
-// correct wiring and tests each pixel's ability to display red, green
-// and blue and to forward data down the line.  By limiting the number
-// and color of LEDs, it's reasonably safe to power a couple meters off
-// the Arduino's 5V pin.  DON'T try that with other code!
-
 #include <Adafruit_DotStar.h>
 // Because conditional #includes don't work w/Arduino sketches...
 #include <SPI.h>         // COMMENT OUT THIS LINE FOR GEMMA OR TRINKET
-//#include <avr/power.h> // ENABLE THIS LINE FOR GEMMA OR TRINKET
+#include <avr/power.h> // ENABLE THIS LINE FOR GEMMA OR TRINKET
+#include <avr/sleep.h>
 
 #define NUMPIXELS 7 // Number of LEDs in strip
 
+#define IRPIN       10
 #define HALLPIN     8
-
-// Here's how to control the LEDs from any two pins:
 #define DATAPIN1    3
 #define CLOCKPIN1   4
 #define DATAPIN2    5
@@ -25,47 +18,36 @@ Adafruit_DotStar strip1 = Adafruit_DotStar(
 
 Adafruit_DotStar strip2 = Adafruit_DotStar(
   NUMPIXELS, DATAPIN2, CLOCKPIN2, DOTSTAR_BRG);
-// The last parameter is optional -- this is the color data order of the
-// DotStar strip, which has changed over time in different production runs.
-// Your code just uses R,G,B colors, the library then reassigns as needed.
-// Default is DOTSTAR_BRG, so change this if you have an earlier strip.
-
-// Hardware SPI is a little faster, but must be wired to specific pins
-// (Arduino Uno = pin 11 for data, 13 for clock, other boards are different).
-//Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DOTSTAR_BRG);
 
 volatile byte revolutions;
-volatile bool detected;
-
-void hallSensor() {
-  detected = true;
-}
-
-void setup() {
-
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
-  clock_prescale_set(clock_div_1); // Enable 16 MHz on Trinket
-#endif
-
-  strip1.begin(); // Initialize pins for output
-  strip2.begin();
-  strip1.show();  // Turn all LEDs off ASAP
-  strip2.show();
-
-  detected = false;
-  revolutions = 0;
-  //attachInterrupt(digitalPinToInterrupt(HALLPIN), hallSensor, LOW);
-}
-
-// Runs 10 LEDs at a time along strip, cycling through red, green and blue.
-// This requires about 200 mA for all the 'on' pixels + 1 mA per 'off' pixel.
+volatile bool hallDetected;
 
 int      head  = 0, tail = -10; // Index of first 'on' and 'off' pixels
 uint32_t color = 0xFF0000;      // 'On' color (starts red)
 
-void loop() {
+void setup() {
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
+  clock_prescale_set(clock_div_1); // Enable 16 MHz on Trinket
+#endif
+  strip1.begin(); // Initialize pins for output
+  strip2.begin();
+  strip1.show();  // Turn all LEDs off 
+  strip2.show();
 
-  if(digitalRead(HALLPIN) == LOW) {
+  hallDetected = false;
+  revolutions = 0;
+
+  // Enable specialized pin intterupts 
+  enableInterruptPin(HALLPIN);
+  enableInterruptPin(IRPIN);
+}
+
+void loop() {
+  // if idling write code before goToSleep()
+  interruptSleep();
+  // normal looping code after goToSleep()
+
+  if(hallDetected) {
     strip1.setPixelColor(head, color); // 'On' pixel at head
     strip1.setPixelColor(tail, 0);     // 'Off' pixel at tail
     strip1.show();                     // Refresh strip
@@ -83,4 +65,41 @@ void loop() {
     }
     if(++tail >= NUMPIXELS) tail = 0; // Increment, reset tail index
   } 
+}
+
+
+// ------ Intterupt Functionality ----- //
+void enableInterruptPin(byte pin) {
+    *digitalPinToPCMSK(pin) |= bit (digitalPinToPCMSKbit(pin));  // enable pin
+    PCIFR  |= bit (digitalPinToPCICRbit(pin)); // clear any outstanding interrupt
+    PCICR  |= bit (digitalPinToPCICRbit(pin)); // enable interrupt for the group
+}
+
+void interruptSleep() {
+  // The ATmega328 has five different sleep states.
+  // SLEEP_MODE_IDLE -the least power savings 
+  // SLEEP_MODE_ADC
+  // SLEEP_MODE_PWR_SAVE
+  // SLEEP_MODE_STANDBY
+  // SLEEP_MODE_PWR_DOWN 
+  
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // Set sleep mode.
+  sleep_enable(); // Enable sleep mode.
+  sleep_mode(); // Enter sleep mode.
+  // Point of interrupt waking and resuming instruction execution
+  sleep_disable(); // Disable sleep mode after waking.
+}
+
+ISR (PCINT0_vect) { // handle pin change interrupt for D8 to D13
+  if(digitalRead(HALLPIN) == LOW) {
+    hallDetected = false;
+  } else {
+    hallDetected = true;
+  }
+} 
+
+ISR (PCINT1_vect) { // handle pin change interrupt for A0 to A5
+}
+
+ISR (PCINT2_vect) { // handle pin change interrupt for D0 to D7 
 }
